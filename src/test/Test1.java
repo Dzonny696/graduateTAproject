ad340ceb35538edcc3dc3306f5bb93bffa8f3191
Nikola Viric <Nikola.Viric@endava.com>
2016-08-05 12:49:33 +0200

    upload first version of project

---
+++ src/test/Test1.java
@@ -0,0 +1,37 @@
+package test;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.server.browserlaunchers.Sleeper;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+
+public class Test1 {
+	
+	private static WebDriver driver;
+	private String URL;
+	
+	
+	 @BeforeClass
+	  public void beforeTest() {
+		  driver = new FirefoxDriver();
+			URL = "http://www.google.com";
+	  }
+	 
+  @Test
+  public void f()
+  {
+	  driver.get(URL);
+	  driver.manage().window().maximize();
+	  Sleeper.sleepTightInSeconds(5);
+  }
+  @AfterClass
+  public void afterClass() {
+	  driver.close();
+  }
+
+ 
+
+}
